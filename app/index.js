674489bbb4d4e5f9d5aa7b83855ef824bd24f17c
inspoy <inspoy@163.com>
2017-03-03 16:17:12 +0800

    - 创建electron工程

---
+++ app/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by inspoy on 2017/3/3.
+ */
+'use strict';
---
+++ main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by inspoy on 2017/3/3.
+ */
+'use strict';
+
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win = null;
+
+const createWindow = function () {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        resizable: false
+    });
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'app/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.on('closed', function () {
+        win = null;
+    });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    app.quit();
+});
